614c966dc8012937a979a245cb990cdd864a5707
sakshib0811 <85578492+sakshib0811@users.noreply.github.com>
2025-11-08 22:35:29 +0530

    Starting with Spring Boot

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/myproject/springboot/demo/myapp/rest/FunRestController.java
@@ -0,0 +1,20 @@
+package com.myproject.springboot.demo.myapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World !";
+    }
+
+    @GetMapping("/sakshi")
+    public String saySakshi(){
+        return "Hello Sakshi All the best and keep Working.";
+    }
+}
