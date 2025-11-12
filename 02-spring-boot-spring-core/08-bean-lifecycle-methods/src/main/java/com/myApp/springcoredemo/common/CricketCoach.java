1b5e7526b29abfec8b7dd74067092fc794fa9f3a
sakshib0811 <85578492+sakshib0811@users.noreply.github.com>
2025-11-13 02:02:48 +0530

    Bean-lifecycle-demo

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/myApp/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,30 @@
+package com.myApp.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    public CricketCoach(){
+        System.out.println("In constructor : " + getClass().getSimpleName());
+    }
+
+    //define init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff() : " + getClass().getSimpleName());
+    }
+
+    //define our destroy method
+    @PreDestroy
+    public void doMyCleanStuff(){
+        System.out.println("In doMyCleanStuff() : " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout(){
+        return "Practice fast bowling for 15 minutes. :-)" ;
+    }
+}
